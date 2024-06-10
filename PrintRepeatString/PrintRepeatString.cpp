3ebc4b7e9dfcf12b65d538ea405af7096ae4b902
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-06-10 23:34:18 +0900

    문자열 반복해서 출력하기

---
+++ PrintRepeatString/PrintRepeatString.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string str;
+	int n;
+	
+	cin >> str >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << str;
+	}
+}
