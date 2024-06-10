5afc2be8d6d801c106bac5609e2178e85d908726
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-06-10 23:38:59 +0900

    대소문자 바꿔서 출력하기

---
+++ PrintLowerUpper/PrintLowerUpper.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    
+    cin >> str;
+    int changeUpDown = 'a' - 'A';
+
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        if (str[i] >= 'A' && str[i] <= 'Z')
+            str[i] += changeUpDown;
+        else
+            str[i] -= changeUpDown;
+        i++;
+    }
+
+    cout << str;
+
+    return 0;
+}
