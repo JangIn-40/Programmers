adec7637ca9f2bc9a58ada201dcd1aadbaaaa1d7
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-06-11 11:40:00 +0900

    홀짝 구분하기

---
+++ ClassifyEvenOdd/ClassifyEvenOdd.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int n;
+
+	cin >> n;
+
+	string classify = (n % 2 == 0) ? " is even" : " is odd";
+	cout << n << classify << endl;
+}
