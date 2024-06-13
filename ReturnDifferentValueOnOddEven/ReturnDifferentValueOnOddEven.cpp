72f31c4e308868a61032e0e56239251569ac145a
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-06-13 21:17:56 +0900

    홀짝에 따라 다른 값 반환하기 재귀함수 적용

--- ReturnDifferentValueOnOddEven/ReturnDifferentValueOnOddEven.cpp
+++ ReturnDifferentValueOnOddEven/ReturnDifferentValueOnOddEven.cpp
@@ -4,24 +4,41 @@
 
 using namespace std;
 
+void sub(int n, int &sum)
+{
+	if (n <= 0)
+		return;
+	else
+	{
+		sum += n;
+		sub(n - 2, sum);
+	}
+}
+
+void multiple(int n, int &sum)
+{
+	if (n <= 0)
+		return;
+	else
+	{
+		sum += n * n;
+		multiple(n - 2, sum);
+	}
+}
+
+// 재귀함수도 사용하고 while도 사용해봤는데 어떤게 더 좋은지 잘 모르겠다
+// 재귀함수는 직관성이 높지만 속도가 느릴 수 있고
+// while문은 재귀함수보다 속도가 높지만 직관성이 떨어질 수 있다.
 int solution(int n)
 {
 	int answer = 0;
 	if (n % 2)
 	{
-		while (n >= 0)
-		{
-			answer += n;
-			n -= 2;
-		}
+		sub(n, answer);
 	}
 	else
 	{
-		while (n >= 0)
-		{
-			answer += n * n;
-			n -= 2;
-		}
+		multiple(n, answer);
 	}
 	
 	return answer;
