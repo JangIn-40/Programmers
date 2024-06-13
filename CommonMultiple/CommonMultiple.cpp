263684c4c13f7b7e7743846369db476df93f3305
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-06-13 20:46:01 +0900

    공배수

---
+++ CommonMultiple/CommonMultiple.cpp
@@ -0,0 +1,17 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(int num, int n, int m)
+{
+	int answer = (num % n == 0 && num % m == 0) ? 1 : 0;
+
+	return answer;
+}
+
+int main()
+{
+	solution(10, 5, 3);
+}
