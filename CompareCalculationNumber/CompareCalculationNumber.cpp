90cc7f91493f085053256d0b8aab8d289ac25227
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-06-13 17:32:13 +0900

    두 수의 연산값 비교하기

---
+++ CompareCalculationNumber/CompareCalculationNumber.cpp
@@ -0,0 +1,26 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(int a, int b) {
+	int answer = 0;
+
+	string str1 = to_string(a);
+	string str2 = to_string(b);
+
+	int comp1 = 2 * a * b;
+	answer = stoi(str1 + str2);
+	if (comp1 > answer)
+	{
+		answer = comp1;
+	}
+
+	return answer;
+}
+
+int main()
+{
+	std::cout << solution(2, 91);
+}
