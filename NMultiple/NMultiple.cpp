d11e0bd51b150e16443478074319cd574997461e
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-06-13 20:38:56 +0900

    n의 배수

---
+++ NMultiple/NMultiple.cpp
@@ -0,0 +1,17 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(int num, int n)
+{
+	int answer = (num % n == 0) ? 1 : 0;
+
+	return answer;
+}
+
+int main()
+{
+	cout << solution(10, 6);
+}
