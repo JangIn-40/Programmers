07495b29a558f54844e0edf6eac6fc16a27d2f52
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-06-13 20:59:41 +0900

    홀짝에 따라 다른 값 반환하기

---
+++ ReturnDifferentValueOnOddEven/ReturnDifferentValueOnOddEven.cpp
@@ -0,0 +1,33 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(int n)
+{
+	int answer = 0;
+	if (n % 2)
+	{
+		while (n >= 0)
+		{
+			answer += n;
+			n -= 2;
+		}
+	}
+	else
+	{
+		while (n >= 0)
+		{
+			answer += n * n;
+			n -= 2;
+		}
+	}
+	
+	return answer;
+}
+
+int main()
+{
+	cout << solution(10);
+}
