f4eaaf7d6398df43e435eaddba00099b0586c194
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-06-14 23:00:07 +0900

    코드 처리하기

---
+++ ProcessCode/ProcessCode.cpp
@@ -0,0 +1,41 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+string solution(string code)
+{
+	string answer = "";
+	bool mode{};
+
+	for (int idx{}; idx < code.length(); ++idx)
+	{
+		if (code[idx] == '1')
+		{
+			mode = !mode;
+			continue;
+		}
+			
+		if (mode && (idx % 2 != 0))
+		{
+			answer += code[idx];
+		}
+		else if (!mode && (idx % 2 == 0))
+		{
+			answer += code[idx];
+		}
+	}
+
+	if (answer == "")
+	{
+		return answer = "EMPTY";
+	}
+
+	return answer;
+}
+
+int main()
+{
+	std::cout << solution("abc1abc1abc");
+}
