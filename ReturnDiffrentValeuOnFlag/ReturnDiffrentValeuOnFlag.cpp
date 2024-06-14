e6d3d98ea2c8c887c926a99585dbd2ab0e44e948
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-06-14 22:22:55 +0900

    flag에 따른 다른 값 반환하기

---
+++ ReturnDiffrentValeuOnFlag/ReturnDiffrentValeuOnFlag.cpp
@@ -0,0 +1,19 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(int a, int b, bool flag)
+{
+	int answer = 0;
+
+	answer = flag ? a + b : a - b;
+
+	return answer;
+}
+
+int main()
+{
+	std::cout << solution(-4, 7, false);
+}
