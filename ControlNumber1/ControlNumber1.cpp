abe4b81f73e127b97a51a9f6cfb00ca053bed98d
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-06-16 14:44:41 +0900

    수 조작하기1

---
+++ ControlNumber1/ControlNumber1.cpp
@@ -0,0 +1,38 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int solution(int n, string control)
+{
+	int answer = 0;
+
+	// case로 하는게 좀더 보기 좋았겠다.
+	for (const char &e : control)
+	{
+		if (e == 'w')
+		{
+			n += 1;
+		}
+		else if(e == 's')
+		{
+			n -= 1;
+		}
+		else if (e == 'd')
+		{
+			n += 10;
+		}
+		else
+		{
+			n -= 10;
+		}
+	}
+
+	answer = n;
+	return answer;
+}
+
+int main()
+{
+	std::cout << solution(0, "wsdawsdassw");
+}
