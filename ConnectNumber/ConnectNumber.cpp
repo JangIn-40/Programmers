320c8f2e20bea8a7c0c100b9623ab6719342d835
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-06-16 14:09:46 +0900

    이어 붙인 수 string사용

---
+++ ConnectNumber/ConnectNumber.cpp
@@ -0,0 +1,24 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int solution(vector<int> num_list)
+{
+	int answer = 0;
+
+	string odd{}, even{};
+	for (const int &e : num_list)
+	{
+		e % 2 == 0 ? even += to_string(e) : odd += to_string(e);
+	}
+
+	answer = stoi(even) + stoi(odd);
+	return answer;
+}
+
+int main()
+{
+	vector<int> num_list{ 5, 7, 8, 3 };
+	std::cout << solution(num_list);
+}
