f6b44a9c5768420e4f99ee584b8cc760d1f6ecff
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-06-17 22:29:16 +0900

    수열과 구간 쿼리 2

---
+++ SequencesAndSpheresQuery/SequencesAndSpheresQuery.cpp
@@ -0,0 +1,43 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+#include <limits>
+using namespace std;
+
+vector<int> solution(vector<int> arr, vector<vector<int>> queries)
+{
+	vector<int> answer;
+	int temp;
+	
+	for (int i = 0; i < queries.size(); ++i)
+	{
+		temp = std::numeric_limits<int>::max();
+		for (int j = queries[i][0]; j <= queries[i][1]; ++j)
+		{
+			if (arr[j] < temp && arr[j] > queries[i][2])
+			{
+				temp = arr[j];
+			}
+		}
+		if (temp == std::numeric_limits<int>::max())
+		{
+			temp = -1;
+		}
+		answer.push_back(temp);
+	}
+
+	return answer;
+}
+
+int main()
+{
+	vector<vector<int>> queries{ {0, 4, 2}, {0, 3, 2}, {0, 2, 2} };
+	vector<int> arr{ 0, 1, 2, 4, 3 };
+	vector<int> answer;
+
+	answer = solution(arr, queries);
+	for (const auto &e : answer)
+	{
+		std::cout << e << std::endl;
+	}
+}
