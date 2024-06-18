f7cfaa9bec90b1f4b49b73533898a2cc2cbdda90
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-06-18 20:00:04 +0900

    콜라츠 수열 만들기

---
+++ CollatzSequence/CollatzSequence.cpp
@@ -0,0 +1,35 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> solution(int n)
+{
+	vector<int> answer;
+
+	while (n != 1)
+	{
+		answer.push_back(n);
+		if (n % 2 == 0)
+		{
+			n /= 2;
+		}
+		else
+		{
+			n = 3 * n + 1;
+		}
+	}
+
+	answer.push_back(n);
+	return answer;
+}
+
+int main()
+{
+	auto e = solution(10);
+
+	for (const auto &i : e)
+	{
+		std::cout << i << " ";
+	}
+}
