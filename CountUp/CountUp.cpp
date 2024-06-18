b6088358c043777d773be31534279313b97214b6
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-06-18 19:43:43 +0900

    카운트 업

---
+++ CountUp/CountUp.cpp
@@ -0,0 +1,26 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> solution(int start_num, int end_num)
+{
+	vector<int> answer;
+
+	for (start_num; start_num <= end_num; start_num++)
+	{
+		answer.push_back(start_num);
+	}
+
+	return answer;
+}
+
+int main()
+{
+	auto e = solution(3, 10);
+
+	for (const auto i : e)
+	{
+		std::cout << i << std::endl;
+	}
+}
