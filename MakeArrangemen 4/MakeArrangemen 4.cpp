dbe5add0760f66c748ccd3f12f34b3b3f9130f02
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-06-19 13:53:17 +0900

    배열 만들기4

---
+++ MakeArrangemen 4/MakeArrangemen 4.cpp
@@ -0,0 +1,38 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> solution(vector<int> arr)
+{
+	vector<int> stk;
+	int i{};
+
+	while(i < arr.size())
+	{
+		if (stk.empty())
+		{
+			stk.push_back(arr[i]);
+			++i;
+		}
+		else if(stk.back() < arr[i])
+		{
+			stk.push_back(arr[i]);
+			++i;
+		}
+		else
+		{
+			stk.pop_back();
+		}
+	}
+	return stk;
+}
+
+int main()
+{
+	auto e = solution({ 1, 4, 2, 5, 3 });
+	for (const auto &i : e)
+	{
+		std::cout << i << std::endl;
+	}
+}
