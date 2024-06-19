34d03bd4e814efd550b2c120f05407f04d6666a4
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-06-19 17:30:05 +0900

    간단한 논리연산

---
+++ SimpleLogicCalculate/SimpleLogicCalculate.cpp
@@ -0,0 +1,19 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+// 논리연산자를 비트연산자로 바꾸어도 가능하네?
+bool solution(bool x1, bool x2, bool x3, bool x4)
+{
+	bool answer = true;
+
+	answer = (x1 || x2) && (x3 || x4);
+
+	return answer;
+}
+
+int main()
+{
+	
+}
