141b887ee4948b4e3d84f9713b9f0442b1e68c7b
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-06-20 21:38:23 +0900

    9로 나눈 나머지

---
+++ Divide9Remain/Divide9Remain.cpp
@@ -0,0 +1,23 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+#include <limits>
+using namespace std;
+
+// 굳이 int로 안바꿔도 된다
+int solution(string number)
+{
+	int answer = 0;
+
+	for (int i = 0; i < number.size(); ++i)
+	{
+		answer += number[i] - '0';
+	}
+
+	return answer % 9;
+}
+
+int main()
+{
+	std::cout << solution("78720646226947352489");
+}
