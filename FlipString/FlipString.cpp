9581cf86488ad9cafbf5becea0294f19444c359f
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-06-20 22:36:40 +0900

    문자열 여러 번 뒤집기

---
+++ FlipString/FlipString.cpp
@@ -0,0 +1,29 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+// reverse와 foreach문으로 코드 확줄일 수 있구나...
+string solution(string my_string, vector<vector<int>> queries)
+{
+	string answer = "";
+
+	for (int i = 0; i < queries.size(); ++i)
+	{
+		for (int j = queries[i][0], k = queries[i][1]; 
+			j <= (queries[i][1] + queries[i][0]) / 2; ++j, --k)
+		{
+			char temp = my_string[j];
+			my_string[j] = my_string[k];
+			my_string[k] = temp;
+		}
+	}
+	answer = my_string;
+	
+	return answer;
+}
+
+int main()
+{
+	std::cout << solution("rermgorpsam", { {2, 3}, {0, 7}, {5, 9}, {6, 10} });	
+}
