94d2ed79e6315f57c9b239674e73c302c49214ce
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-06-21 18:01:04 +0900

    배열 만들기 5

---
+++ MakeArray 5/MakeArray 5.cpp
@@ -0,0 +1,35 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> solution(vector<string> intStrs, int k, int s, int l)
+{
+	vector<int> answer;
+
+	for (int i = 0; i < intStrs.size(); ++i)
+	{
+		int temp{};
+		for (int j = s; j < l + s; ++j)
+		{
+			temp *= 10;
+			temp += intStrs[i][j] - '0';
+		}
+		if (temp > k)
+		{
+			answer.push_back(temp);
+		}
+		
+		for (auto &e : answer)
+		{
+			cout << e << endl;
+		}
+	}
+
+	return answer;
+}
+
+int main()
+{
+	solution({ "0123456789", "9876543210", "9999999999999" }, 50000, 5, 5);
+}
