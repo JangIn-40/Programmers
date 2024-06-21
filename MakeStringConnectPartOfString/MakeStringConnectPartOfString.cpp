0cfceb890839ae5fd716c1328653a109f2c8db71
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-06-21 18:30:43 +0900

    부분 문자열 이어 붙여 문자열 만들기

---
+++ MakeStringConnectPartOfString/MakeStringConnectPartOfString.cpp
@@ -0,0 +1,25 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+string solution(vector<string> my_strings, vector<vector<int>> parts)
+{
+	string answer = "";
+
+	for (int i = 0; i < parts.size(); ++i)
+	{
+		for (int j = parts[i][0]; j <= parts[i][1]; ++j)
+		{
+			answer += my_strings[i][j];
+		}
+	}
+
+	return answer;
+}
+
+int main()
+{
+	solution({ "progressive", "hamburger", "hammer", "ahocorasick" }, 
+		{ {0, 4}, {1, 2}, {3, 5}, {7, 7} });
+}
