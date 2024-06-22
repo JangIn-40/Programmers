5cb78370a056dbbb85bc3fd53133b7411d366af9
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-06-22 12:33:19 +0900

    접미사 배열

---
+++ SuffixArray/SuffixArray.cpp
@@ -0,0 +1,23 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+vector<string> solution(string my_string)
+{
+	vector<string> answer;
+
+	for (int i = 0; i < my_string.size(); ++i)
+	{
+		answer.emplace_back(my_string.begin() + i, my_string.end());
+	}
+	
+	std::sort(answer.begin(), answer.end());
+	return answer;
+}
+
+int main()
+{
+
+}
