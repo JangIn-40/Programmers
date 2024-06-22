b52b40cfbba04aa6778da7cd0bd24c3fb586d72f
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-06-22 18:33:55 +0900

    문자열의 앞의 n글자

---
+++ NLettersInFrontOfString/NLettersInFrontOfString.cpp
@@ -0,0 +1,18 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+string solution(string my_string, int n)
+{
+	string answer = "";
+	
+	answer = my_string.substr(0, n);
+
+	return answer;
+}
+
+int main()
+{
+	std::cout << solution("ProgrammerS123", 11);
+}
