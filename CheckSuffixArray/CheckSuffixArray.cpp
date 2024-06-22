bff798bbb75b722c3d4b1be48051549a33b06f43
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-06-22 13:02:21 +0900

    접미사인지 확인하기

---
+++ CheckSuffixArray/CheckSuffixArray.cpp
@@ -0,0 +1,24 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int solution(string my_string, string is_suffix)
+{
+	int answer = 0;
+
+	for (int i = 0; i < my_string.size(); ++i)
+	{
+		if (is_suffix == my_string.substr(i))
+		{
+			return answer = 1;
+		}
+	}
+
+	return answer;
+}
+
+int main()
+{
+	std::cout << solution("banana", "nan");
+}
