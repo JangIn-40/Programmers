69473f1af01186ffcecb57e44b0dbf00ac972cb7
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-06-23 14:24:02 +0900

    접두사인지 확인하기

---
+++ CheckPrefix/CheckPrefix.cpp
@@ -0,0 +1,25 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+// for문 안써도 된다!
+int solution(string my_string, string is_prefix)
+{
+	int answer = 0;
+
+	for (int i = 0; i < my_string.size(); ++i)
+	{
+		if (is_prefix == my_string.substr(0, i))
+		{
+			return answer = 1;
+		}
+	}
+
+	return answer;
+}
+
+int main()
+{
+	solution("banana", "ban");
+}
