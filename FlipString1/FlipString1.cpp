ba507aa30b37a670a34e1aa26dd652a79a195765
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-06-23 14:57:21 +0900

    문자열 뒤집기

---
+++ FlipString1/FlipString1.cpp
@@ -0,0 +1,22 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+// std::reverse()에서 last는 범위에 포함되지 않는다.
+// 이터레이터 end가 어딜 가리키는지 생각해보면 말이 되는것 같다
+string solution(string my_string, int s, int e)
+{
+	string answer = "";
+
+	reverse(my_string.begin() + s, my_string.begin() + e + 1);
+
+	answer = my_string;
+	return answer;
+}
+
+int main()
+{
+	cout << solution("Progra21Sremm3", 6, 12);
+}
