2a95ba418af47c40acedc46df0b1b9ca0ece072f
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-06-24 10:57:18 +0900

    문자 개수 세기

---
+++ CountStringNumber/CountStringNumber.cpp
@@ -0,0 +1,34 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+// my_string[i] - 'G'이것보다 my_string[i] - 'a' + 26이 더 보기 좋은듯하다
+vector<int> solution(string my_string)
+{
+	vector<int> answer(52, 0);
+
+	for (int i = 0; i < my_string.size(); ++i)
+	{
+		if (my_string[i] <= 'Z')
+		{
+			answer[my_string[i] - 'A']++;
+		}
+		else
+		{
+			answer[my_string[i] - 'G']++;
+		}
+	}
+
+	return answer;
+}
+
+int main()
+{
+	auto sol = solution("Programmers");
+
+	for (auto &e : sol)
+	{
+		std::cout << e << " ";
+	}
+}
