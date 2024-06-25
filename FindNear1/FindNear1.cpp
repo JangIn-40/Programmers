45627438a105c051fa48fa6bd811cc9485aca600
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-06-25 15:57:36 +0900

    가가운 1 찾기

---
+++ FindNear1/FindNear1.cpp
@@ -0,0 +1,20 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<int> arr, int idx)
+{
+	int answer = -1;
+
+	for (int i = idx; i < arr.size(); ++i)
+	{
+		if (arr[i] == 1)
+		{
+			return answer = i;
+		}
+	}
+
+	return answer;
+}
