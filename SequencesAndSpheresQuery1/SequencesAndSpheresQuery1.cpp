40888b44f03bcacbf65f106a525d7e2ad79a2efb
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-06-30 08:34:20 +0900

    수열과 구간 쿼리1

---
+++ SequencesAndSpheresQuery1/SequencesAndSpheresQuery1.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> solution(vector<int> arr, vector<vector<int>> queries)
+{
+	vector<int> answer;
+
+	for (const auto &e : queries)
+	{
+		for (int i = e[0]; i <= e[1]; ++i)
+		{
+			++arr[i];
+		}
+	}
+
+	answer = arr;
+	return answer;
+}
