1fd5d540f8525821f0402648902362fc1d2cd86a
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-07-01 17:24:39 +0900

    길이에 따른 연산

---
+++ CalculateBaseOnLength/CalculateBaseOnLength.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int solution(vector<int> num_list)
+{
+	int answer = 0;
+	if (num_list.size() > 10)
+	{
+		for (const auto &e : num_list)
+		{
+			answer += e;
+		}
+	}
+	else
+	{
+		answer = 1;
+		for (const auto &e : num_list)
+		{
+			answer *= e;
+		}
+	}
+
+	return answer;
+}
