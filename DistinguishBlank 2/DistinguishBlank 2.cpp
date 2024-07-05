906c2f79a25c20b4e830b12a27a51ecb1b6ea102
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-07-05 11:19:03 +0900

    공백으로 구분하기 2

---
+++ DistinguishBlank 2/DistinguishBlank 2.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<string> solution(string my_string)
+{
+    vector<string> answer;
+
+	int start{}, end{};
+	while (my_string.find(' ', start) != my_string.npos)
+	{
+		end = my_string.find(' ', start);
+		if(end != start)
+			answer.push_back(my_string.substr(start, end - start));
+		start = end + 1;
+	}
+
+	if (start < my_string.size())
+	{
+		answer.push_back(my_string.substr(start));
+	}
+	
+	return answer;
+}
+
+int main()
+{
+    solution("    programmers ");
+}
