3ba23e49360c85da44f2f4948e7d7420b60925b2
JangIn-40 <156999923+JangIn-40@users.noreply.github.com>
2024-07-17 09:02:32 +0900

    이차원 배열 대각선 순회하기

---
+++ Two-dimensionalArrayDiagonalTraversal/Two-dimensionalArrayDiagonalTraversal.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> board, int k) {
+    int answer = 0;
+
+    for (int i = 0; i < k + 1 && i < board.size(); ++i)
+    {
+        for (int j = 0; j + i < k + 1 && j < board[i].size(); ++j)
+        {
+            answer += board[i][j];
+        }
+    }
+    return answer;
+}
