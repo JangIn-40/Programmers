68a6ef49a2dc2a7aa706b22dae93ef3b6e2e3520
JangIn-40 <jih105802@gmail.com>
2024-07-21 09:54:47 +0900

    공 던지기

---
+++ LV0/introduction/ThrowBall/ThrowBall.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> numbers, int k)
+{
+    int answer = 0;
+
+    answer = numbers[(k * 2 - 2) % numbers.size()];
+
+    return answer;
+}
+
+int main()
+{
+	solution({ 1, 2, 3, 4, 5 }, 3);
+}
