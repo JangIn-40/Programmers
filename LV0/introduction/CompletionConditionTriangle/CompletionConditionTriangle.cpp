02df3df1c9c193233ee54236c7376ef6392c72d5
JangIn-40 <jih105802@gmail.com>
2024-07-25 10:59:12 +0900

    삼각형의 완성조건 (1)

---
+++ LV0/introduction/CompletionConditionTriangle/CompletionConditionTriangle.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> sides)
+{
+	int answer = 0;
+	sort(sides.begin(), sides.end());
+
+	answer = sides[0] + sides[1] > sides[2] ? 1 : 2;
+	return answer;
+}
