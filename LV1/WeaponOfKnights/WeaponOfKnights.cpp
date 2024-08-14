76d36f3a9d4e182e5cf2433c340255aca0ae067d
JangIn-40 <jih105802@gmail.com>
2024-08-14 22:23:43 +0900

    기사단원의 무기

---
+++ LV1/WeaponOfKnights/WeaponOfKnights.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int number, int limit, int power)
+{
+	int answer = 0;
+    std::vector<int> cd;
+
+    for (int i = 1; i <= number; ++i)
+    {
+        int num{};
+        for (int j = 1; j * j <= i; ++j)
+        {
+            if (i % j == 0)
+            {
+                ++num;
+            }
+        }
+        cd.emplace_back(num);
+    }
+
+    for (int& num : cd)
+    {
+        if (num > limit)
+        {
+            num = power;
+        }
+        answer += num;
+    }
+
+	return answer;
+}
