3749f9b587f2f03be24d3d0f76d08fafd47b5b4a
JangIn-40 <jih105802@gmail.com>
2024-08-23 11:43:00 +0900

    정수 내림차순으로 배치하기

---
+++ LV1/PlaceDescendingOrderInteger/PlaceDescendingOrderInteger.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+long long solution(long long n)
+{
+    long long answer = 0;
+    string num = to_string(n);
+
+    sort(num.begin(), num.end(), greater<char>());
+
+    answer = stoll(num);
+    return answer;
+}
+
+int main()
+{
+    cout << solution(118372);
+}
