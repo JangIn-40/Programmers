9d497234931c8d7970b38269320a0f710eb15feb
JangIn-40 <jih105802@gmail.com>
2024-08-30 15:31:46 +0900

    예산

---
+++ LV1/Budget/Budget.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> d, int budget) 
+{
+    int answer = 0;
+
+    sort(d.begin(), d.end());
+    for (const int& num : d)
+    {
+        budget -= num;
+        if (budget < 0)
+        {
+            break;
+        }
+        else
+        {
+            ++answer;
+        }
+    }
+
+    return answer;
+}
